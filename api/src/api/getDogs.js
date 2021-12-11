facf9b12d9c0c5e265d71a5390e703f9a0dab895
unknown <cervantes@ciencias.unam.mx>
2021-12-11 00:02:54 -0600

    C:/Program Files/Git/dogs ok

--- api/src/api/getDogs.js
+++ api/src/api/getDogs.js
@@ -1,5 +1,25 @@
 const axios = require('axios');
- //aqui si
 const { API_KEY } = process.env;
-const allDogsApi =  (axios.get('https://api.thedogapi.com/v1/breeds'));
-module.exports = allDogsApi;
+const { Dog} = require('../db');
+
+
+async function getAllDogs(){
+    const allDogsApi =  await (axios.get(`https://api.thedogapi.com/v1/breeds?api_key=${API_KEY}`))
+
+    allDogsApi.data.map(dog => {
+        Dog.findOrCreate({
+            where: {
+                id: dog.id,
+                name: dog.name,
+                height: dog.height.metric,
+                weight: dog.weight.metric,
+                life_span: dog.life_span,
+                image: dog.image.url
+            }
+        })
+    })
+}
+
+
+
+module.exports = {getAllDogs};
--- api/src/routes/dogs.js
+++ api/src/routes/dogs.js
@@ -1,33 +1,28 @@
 const { Router } = require('express');
-const { API_KEY } = process.env;
-const axios = require('axios');
-const { Dog, Temperament} = require('../db');
+const { Op } = require("sequelize");
+const { Dog } = require('../db');
+const {getAllDogs} = require('../api/getDogs')
 
 const router = Router();
 
 router.get('/dogs', async (req, res, next) => {
-    const allDogsApi = await getAllDogs()
-    console.log('La data ha sido descargada de la API de perros')
-    allDogsApi ? res.status(200).send(allDogsApi) : res.status(404)
-})
-
-async function getAllDogs(){
-    const allDogsApi =  await (axios.get(`https://api.thedogapi.com/v1/breeds?api_key=${API_KEY}`))
-
-    allDogsApi.data.map(dog => {
-        Dog.findOrCreate({
+    const {name} = req.query;
+    await getAllDogs()
+    if(name){
+        let unDogo = await Dog.findAll({
             where: {
-                id: dog.id,
-                name: dog.name,
-                height: dog.height.metric,
-                weight: dog.weight.metric,
-                life_span: dog.life_span,
-                image: dog.image.url
+                name: {
+                    [Op.iLike]: `%${name}%`
+                }
             }
         })
-    })
-    return allDogsApi.data
-}
+        unDogo.length > 0 ? res.status(200).json(unDogo[0].dataValues) : res.status(404).send('Dogo no encontrado')
+    }else{
+        const alldogs = await Dog.findAll();
+        alldogs ? res.status(200).json(alldogs): res.status(404).send("Error al enviar getAllDogs")
+    }
+
+})
 
 module.exports = router;
 
